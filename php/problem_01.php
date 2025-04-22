8be72599337189257d875d979618a550e676575c
Boominathan <eboominathan@gmail.com>
2025-04-22 18:26:16 +0530

    1. Two Sum

---
+++ php/problem_01.php
@@ -0,0 +1,23 @@
+<?php
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target)
+    {
+        $length = count($nums);
+
+        for ($i = 0; $i < $length; $i++) {
+            for ($j = $i + 1; $j < $length; $j++) {
+                if ($nums[$i] + $nums[$j] == $target) {
+                    return [$i, $j];
+                }
+            }
+        }
+
+        return []; // Return empty array if no match found
+    }
+}
